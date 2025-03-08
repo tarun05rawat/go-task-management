3a068491529c6623ca33f39b3310ab87bbd63b2b
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2025-03-07 20:09:27 -0500

    added JWT auth

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/tarun05rawat/go-task-management/database"
 	"github.com/tarun05rawat/go-task-management/model"
 	"golang.org/x/crypto/bcrypt"
@@ -45,5 +49,55 @@ func Signup(c *gin.Context) {
 		"username": user.Username,
 		"email":    user.Email,
 	}})
+}
 
+func Login(c *gin.Context) {
+	// get the email and password off req body
+	var body struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to read body"})
+		return
+	}
+
+	// Look up requested user
+	var user model.User
+	database.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid email/password"})
+		return
+	}
+
+	// Compare sent in password to user's actual password hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(body.Password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid email/password"})
+		return
+	}
+
+	//generate a jwt token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(os.Getenv("SECRET"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to create token"})
+		return
+	}
+
+	fmt.Println(tokenString, err)
+
+	//send it back
+	c.JSON(http.StatusOK, gin.H{
+		"token": tokenString,
+	})
 }
